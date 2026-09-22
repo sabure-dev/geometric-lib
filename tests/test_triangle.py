bb0e74f03882ecb3b156704d69060bb87bbdb6a1
sabure-dev <dev.sabure@gmail.com>
2026-09-22 22:43:52 +0300

    test: add test for triangle module

---
+++ tests/test_triangle.py
@@ -0,0 +1,62 @@
+import unittest
+
+from triangle import area, perimeter
+
+
+class TriangleTestCase(unittest.TestCase):
+
+    def test_area_typical(self):
+        res = area(4.0, 3.0)
+        self.assertEqual(res, 6.0)
+
+    def test_area_zero_height(self):
+        res = area(10, 0)
+        self.assertEqual(res, 0)
+
+    def test_area_unit_values(self):
+        res = area(1, 1)
+        self.assertEqual(res, 0.5)
+
+    def test_area_float_values(self):
+        res = area(5.0, 2.5)
+        self.assertAlmostEqual(res, 6.25)
+
+    def test_area_negative_values_rejected(self):
+        with self.assertRaises(ValueError):
+            area(-4, 3)
+
+    def test_area_string_type_raises(self):
+        with self.assertRaises(TypeError):
+            area("4", "3")
+
+    def test_area_none_type_raises(self):
+        with self.assertRaises(TypeError):
+            area(None, 3)
+
+    def test_perimeter_typical(self):
+        res = perimeter(3.0, 4.0, 5.0)
+        self.assertEqual(res, 12.0)
+
+    def test_perimeter_string_type_should_raise(self):
+        with self.assertRaises(TypeError):
+            perimeter("3", "4", "5")
+
+    def test_perimeter_none_type_raises(self):
+        with self.assertRaises(TypeError):
+            perimeter(None, 4, 5)
+
+    def test_perimeter_equilateral(self):
+        res = perimeter(5, 5, 5)
+        self.assertEqual(res, 15)
+
+    def test_perimeter_float_sides(self):
+        res = perimeter(2.5, 3.5, 4.0)
+        self.assertAlmostEqual(res, 10.0)
+
+    def test_perimeter_invalid_triangle_rejected(self):
+        with self.assertRaises(ValueError):
+            perimeter(1, 1, 100)
+
+
+if __name__ == '__main__':
+    unittest.main()
